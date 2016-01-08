b55cef54de75c95279c11531059696d59039d01b
Popa Alex <popa.alex1@gmail.com>
2016-01-08 07:29:02 -0800

    first commit to the project ,it's more of a blueprint ,not much work done so far..

---
+++ project/src/main/java/ro/sci/bitwire/BitwireApplication.java
@@ -0,0 +1,12 @@
+package ro.sci.bitwire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BitwireApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BitwireApplication.class, args);
+	}
+}
---
+++ project/src/main/java/ro/sci/bitwire/service/UserService.java
@@ -0,0 +1,5 @@
+package ro.sci.bitwire.service;
+
+public class UserService {
+
+}
