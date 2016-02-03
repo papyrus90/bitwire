53f0935c50b87edfca3b763f8bc2e860c84a4a98
Vlad Riti <vladriti@gmail.com>
2016-02-03 23:46:20 +0200

    commit 3
    updated the header
    finished the products page

--- products.php
+++ products.php
@@ -5,44 +5,48 @@
         <link rel="stylesheet" href="css/normalize.css" type="text/css"/>
         <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
         <link rel="stylesheet" href="css/style.css" type="text/css"/>
-        <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-        <link href='https://fonts.googleapis.com/css?family=Fira+Sans' rel='stylesheet' type='text/css'>
-        <title>Bitwire - Social Media Management</title>    
+        <script type="text/javascript" src="js/myscript.js"></script>
+        <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+        <script type="text/javascript" src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+        <link rel="stylesheet" href='https://fonts.googleapis.com/css?family=Fira+Sans' rel='stylesheet' type='text/css'>
+        <title>Bitwire - Social Media Management</title>
     </head>
     <body>
+
         <div class="row">
             <div class="upper-wrapper">
-                <div class="products-main" "col-xs-12" "col-sm-12" "col-md-12" "col-lg-12">
+                <div class="products-main" "col-md-12">
                     <?php include 'header.php'; ?>
-                    <div class="btn-wrapper">
+                    <div class="row row-center">
                         <div class="btn-group">
-                            <div class="btn-group">
-                                <button type="button" class="btn btn-primary" id="marketers">Marketers</button>
-                            </div>
-                            <div class="btn-group">
-                                <button type="button" class="btn btn-primary" id="agencies">Agencies</button>
-                            </div>
+                            <button class="btn btn-info btn-marketers">Marketers</button>
+                            <button class="btn btn-info btn-agencies">Agencies</button>
                         </div>
                         <div class="products-main-title">
                             <h1>Join the 2+ milion marketers</h1>
                             <p>already using Bitwire</p>
-                        </div>    
-                    </div>
-                </div>    
-                <div class="lower-main" "col-xs-12" "col-sm-12" "col-md-12" "col-lg-12">
-                    <div class=lower-main-title>
-                        <h2>Join our network and enjoy all the benefits!</h2>
-                        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing Lorem ipsum dolor sit amet</p>
-                    </div>
-                    <div class=sign-up-button-wrapper>
-                        <a href="#">
-                            <button class="btn btn-danger btn-danger-home btn-lg">Sign-up - It's free</button>
-                        </a>
+                        </div>
                     </div>
                 </div>
-                <div class=footer-artwork col-xs-12 col-sm-12 col-md-12 col-lg-12>
-                    <img src="images/bg-lower-signup.png">
-                </div>    
-            </div>     
-    </body>
+            </div>
+        </div>
+    <div class="container">
+        <div class="row padded"></div>
+        <div class="row">
+            <div class="col-md-2 colored"></div>
+            <div class="col-md-3">
+                <img src="../images/hand-clock.png">
+            </div>
+            <div class="col-md-5">
+                <br>
+                <h3>Schedule your posts for later</h3>
+                <p>Bitwire shares your content at the best possible times throughout the day so that your followers and fans see your updates more often</p>
+                <br>
+                <p style="color: #e9354a;">Get the most out of each post</p>
+            </div>
+            <div class="col-md-2 colored"></div>
+        </div>
+        <div class="row padded"></div>
+    </div>
+    <?php include 'login.php'; ?>
+</body>
